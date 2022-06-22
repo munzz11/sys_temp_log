4d1d85582cdf6698f495722622fa17fe69ad0000
Avery Muñoz <45202950+munzz11@users.noreply.github.com>
2022-06-22 16:03:20 -0400

    Update sys_temp_logger.py
    formatting

--- sys_temp_logger.py
+++ sys_temp_logger.py
@@ -30,65 +30,65 @@ try:
 	write_api.write(bucket=bucket, record=p)
 	
 except:
-  print("Invalid Output")
+  	print("Invalid Output")
 	
 #Zone 1 
 
 try:
-  cmd = 'cat /sys/class/thermal/thermal_zone1/temp'
-  output = subprocess.run(cmd, shell=True, capture_output=True)
-  stderr = output.stderr.decode('utf-8')
-  stdout = float(output.stdout.decode('utf-8'))
-  temp = round(stdout/1000, 1)
+	cmd = 'cat /sys/class/thermal/thermal_zone1/temp'
+	output = subprocess.run(cmd, shell=True, capture_output=True)
+	stderr = output.stderr.decode('utf-8')
+	stdout = float(output.stdout.decode('utf-8'))
+	temp = round(stdout/1000, 1)
 
-  p = Point('Tempatures').tag('Type', 'acpitz').field('Degrees C', temp)
-  write_api.write(bucket=bucket, record=p)
+	p = Point('Tempatures').tag('Type', 'acpitz').field('Degrees C', temp)
+	write_api.write(bucket=bucket, record=p)
 
 except:
-  print("Invalid Output")
+  	print("Invalid Output")
 	
 #Zone 2
 
 try:
-  cmd = 'cat /sys/class/thermal/thermal_zone2/temp'
-  output = subprocess.run(cmd, shell=True, capture_output=True)
-  stderr = output.stderr.decode('utf-8')
-  stdout = float(output.stdout.decode('utf-8'))
-  temp = round(stdout/1000, 1)
+	cmd = 'cat /sys/class/thermal/thermal_zone2/temp'
+	output = subprocess.run(cmd, shell=True, capture_output=True)
+	stderr = output.stderr.decode('utf-8')
+	stdout = float(output.stdout.decode('utf-8'))
+	temp = round(stdout/1000, 1)
 
-  p = Point('Tempatures').tag('Type', 'pch_skylake').field('Degrees C', temp)
-  write_api.write(bucket=bucket, record=p)
+	p = Point('Tempatures').tag('Type', 'pch_skylake').field('Degrees C', temp)
+	write_api.write(bucket=bucket, record=p)
 
 except:
-  print("Invalid Output")
+  	print("Invalid Output")
 	
 #Zone 3
 
 try:
-  cmd = 'cat /sys/class/thermal/thermal_zone3/temp'
-  output = subprocess.run(cmd, shell=True, capture_output=True)
-  stderr = output.stderr.decode('utf-8')
-  stdout = float(output.stdout.decode('utf-8'))
-  temp = round(stdout/1000, 1)
+	cmd = 'cat /sys/class/thermal/thermal_zone3/temp'
+	output = subprocess.run(cmd, shell=True, capture_output=True)
+	stderr = output.stderr.decode('utf-8')
+	stdout = float(output.stdout.decode('utf-8'))
+	temp = round(stdout/1000, 1)
 
-  p = Point('Tempatures').tag('Type', 'iwlwifi_1').field('Degrees C', temp)
-  write_api.write(bucket=bucket, record=p)
+	p = Point('Tempatures').tag('Type', 'iwlwifi_1').field('Degrees C', temp)
+	write_api.write(bucket=bucket, record=p)
 
 except:
-  print("Invalid Output")
+  	print("Invalid Output")
 	
 #Zone 4
 
 try:
-  cmd = 'cat /sys/class/thermal/thermal_zone4/temp'
-  output = subprocess.run(cmd, shell=True, capture_output=True)
-  stderr = output.stderr.decode('utf-8')
-  stdout = float(output.stdout.decode('utf-8'))
-  temp = round(stdout/1000, 1)
+	cmd = 'cat /sys/class/thermal/thermal_zone4/temp'
+	output = subprocess.run(cmd, shell=True, capture_output=True)
+	stderr = output.stderr.decode('utf-8')
+	stdout = float(output.stdout.decode('utf-8'))
+	temp = round(stdout/1000, 1)
 
-  p = Point('Tempatures').tag('Type', 'x86_pkg_temp').field('Degrees C', temp)
-  write_api.write(bucket=bucket, record=p)
+	p = Point('Tempatures').tag('Type', 'x86_pkg_temp').field('Degrees C', temp)
+	write_api.write(bucket=bucket, record=p)
 
 except:
-  print("Invalid Output")
+  	print("Invalid Output")
 
